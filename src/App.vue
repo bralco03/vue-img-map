3bb9b61ad0acd0a25862c756b03e7585b8049f24
bralco03 <bralco03@hotmail.com>
2022-07-13 00:37:56 -0600

    init: project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex">
+        <h2>Graphic IMG practice app</h2>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <imageMap />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import imageMap from './components/imageMap.vue';
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+  name: 'App',
+
+  components: {imageMap},
+
+  data: () => ({
+    //
+  })
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;1,400&display=swap');
+* {
+  font-family: 'Open Sans', sans-serif;
+}
+h2 {
+  width: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-weight: 300;
 }
 </style>
---
+++ src/components/imageMap.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <img
+      alt="img"
+      height="500"
+      width="481"
+      src="../assets/eggImg.jpg"
+      usemap="#workmap" />
+    <map name="workmap">
+      <area shape="rect" coords="0,0,100,100" alt="egg" :href="eggRef" />
+    </map>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'imageMap',
+  data() {
+    const eggRef =
+      'https://www.google.com.mx/search?q=egg&hl=es-419&tbm=isch&sxsrf=ALiCzsY-QvtLn0-9yaGWMYMvypme8yYU9g%3A1657692064361&source=hp&biw=1366&bih=649&ei=oF_OYp6SEO6ekPIPsPqekAs&iflsig=AJiK0e8AAAAAYs5tsC1P6NQQdYIXF8VnbHl10-M-wXlf&ved=0ahUKEwie4oyVmPX4AhVuD0QIHTC9B7IQ4dUDCAc&uact=5&oq=egg&gs_lcp=CgNpbWcQA1AMWK4BYPkBaABwAHgAgAEAiAEAkgEAmAEAoAEBqgELZ3dzLXdpei1pbWewAQA&sclient=img';
+    return {eggRef};
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+area:hover {
+  cursor: pointer;
+}
+</style>
